c50fc56088d7fb6eaf2d32218576699caa5429b4
Patrick Dang <pdang2@mail.greenriver.edu>
2021-02-21 00:13:26 -0500

    added controler page

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the auto autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('Debug',3);
+
+//Define a default route (home page)
+$f3->route('GET /', function(){
+    echo 'recipe home page';
+});
+
+//Run fat free
+$f3->run();
